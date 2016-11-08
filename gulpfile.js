88ed4cb105294a59bef512b11f56871b54bd79e5
Andrei Dmytruk <mallboroks@mail.ru>
2016-11-08 23:27:59 +0200

    18

--- gulpfile.js
+++ gulpfile.js
@@ -68,7 +68,7 @@ gulp.task('scripts', function() {
 });*/
 
 
-gulp.task('watch', ['browser-sync', 'css', 'scripts'], function() {
+gulp.task('watch', ['browser-sync', 'scss', 'css', 'html', 'js', 'scripts'], function() {
   gulp.watch('app/sass/**/*.scss', ['scss']); // Наблюдение за scss файлами в папке sass
   gulp.watch('app/css/**/*.css', ['css']);
   gulp.watch('app/*.html', ['html'], browserSync.reload); // Наблюдение за HTML файлами в корне проекта
@@ -91,19 +91,7 @@ gulp.task('img', function() {
 });
 
 
-gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
-  var buildCss = gulp.src([ // Переносим CSS стили в продакшен
-  'app/css/main.css',
-  'app/css/libs.min.css'
-  ])
-  .pipe(gulp.dest('dist/css'))
-  var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
-  .pipe(gulp.dest('dist/fonts'))
-  var buildJs = gulp.src('app/js/dist/**/*') // Переносим скрипты в продакшен
-  .pipe(gulp.dest('dist/js'))
-  var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
-  .pipe(gulp.dest('dist'));
-});
+gulp.task('build', ['scss', 'css', 'html', 'js', 'scripts']);
 
 gulp.task('default', ['watch']);
 
