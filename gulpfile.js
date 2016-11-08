9f5b8ffd4439858e3beba26e324a1181660533d9
Andrei Dmytruk <mallboroks@mail.ru>
2016-11-08 17:26:07 +0200

    18

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@ var gulp = require('gulp'),
   cssnano = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
   rename = require('gulp-rename'), // Подключаем библиотеку для переименования
   del = require('del'), // Подключаем библиотеку для удаления файлов и папок
-  imagemin    = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
-  pngquant    = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
-  cache       = require('gulp-cache') // Подключаем библиотеку кеширования
+  imagemin = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+  pngquant = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+  cache = require('gulp-cache') // Подключаем библиотеку кеширования
   autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
 
 
